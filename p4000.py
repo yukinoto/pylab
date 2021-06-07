8dd6ce050f1c4b4631bc2cb321220cbc09662f79
yukinoto <2733687567@qq.com>
2021-06-08 06:36:21 +0800

    .

--- p4000.py
+++ p4000.py
@@ -1,15 +1,27 @@
 import sys
 
-def fnth(n):
-	if n==1:
-		return [0,1]
-	else:
-		if n%2==1:
-			ans=fnth(n-1)
-			return [ans[1],ans[0]+ans[1]]
+def fnth(n,mod=None):
+	if mod==None:
+		if n==1:
+			return [0,1]
 		else:
-			ans=fnth(n//2)
-			return [ans[0]**2+ans[1]**2,2*ans[0]*ans[1]+ans[1]**2]
+			if n%2==1:
+				ans=fnth(n-1)
+				return [ans[1],ans[0]+ans[1]]
+			else:
+				ans=fnth(n//2)
+				return [ans[0]**2+ans[1]**2,2*ans[0]*ans[1]+ans[1]**2]
+	else:
+		if n==1:
+			return [0,1]
+		else:
+			if n%2==1:
+				ans=fnth(n-1,mod)
+				return [ans[1],(ans[0]+ans[1])%mod]
+			else:
+				ans=fnth(n//2,mod)
+				return [(ans[0]**2+ans[1]**2)%mod,(2*ans[0]*ans[1]+ans[1]**2)%mod]
 
-def fi(n):
-	return fnth(n)[1]
+def fi(n,mod=None):
+	return fnth(n,mod)[1]
+
